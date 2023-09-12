2668377b8bbd6c82ed3e438421e46ef680bca21d
CamachoKyan <124410885+CamachoKyan@users.noreply.github.com>
2023-09-11 19:29:57 -0700

    Add files via upload

--- Parcial1/JS/practica3.js
+++ Parcial1/JS/practica3.js
@@ -8,14 +8,33 @@ $(document).ready(function(){
         var nombre_imagen = $(this).attr('data-id')
 
         if(estado == "0"){
+            if(carta1 !=""  && carta2 != ""){
+                console.log("Las cartas son diferentes");
+                if(carta1.attr('data-id') != carta2.attr('data-id')){
+                    console.log("Tapa Cartas Diferentes")
+                    carta1.attr('src','Imagenes/Craft.png')
+                    carta2.attr('src','Imagenes/Craft.png')
+                    carta1.attr('data-estado','0')
+                    carta2.attr('data-estado','0')
+                }else{
+                    console.log("Las cartas son iguales")
+                    carta1.attr('data-estado','1')
+                    carta2.attr('data-estado','1')
+                    par=false
+                }
+                carta1=""
+                carta2=""
+            }
             console.log("estado de la carta:" + estado)
             if(carta1 == ""){
                 carta1 = $(this);
                 carta1.attr('src','Imagenes/' + nombre_imagen);
+                carta1.attr('data-estado','1')
                 console.log("seasigno carta #1")
             } else{
                     carta2 = $(this);
                     carta2.attr('src','Imagenes/' + nombre_imagen);
+                    carta2.attr('data-estado','1')
                     console.log("seasigno carta #2")
         }
     }
@@ -30,5 +49,8 @@ $(document).ready(function(){
         }    
     }
     
+    if( total_pares>=8){
+        alert("Has Ganado")
+    }
     });
 });
