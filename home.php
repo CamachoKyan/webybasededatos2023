04814f29ebc60d7672746c804818cfac4e9f3534
CamachoKyan <124410885+CamachoKyan@users.noreply.github.com>
2023-10-16 19:28:11 -0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    <script
+  src="https://code.jquery.com/mobile/1.5.0-rc1/jquery.mobile-1.5.0-rc1.js"
+  integrity="sha256-ozaZ7ZptnEEuLpoEojfxBjOKXHoaQUb/DLnsTUEkIfs="
+  crossorigin="anonymous"></script>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container-fluid">
+      
+        <a class="navbar-brand ms-lg-2 fs-4 fw-bold" href="#Inicio"><img class="rounded-circle" src="https://unavatar.io/youtube/alvamajoclips" width="50px" height="50px alt"><span class="ms-lg-2 ms-md-2 ms-sm-2 ms-xs-2">Amante de Guinxu</span></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+         
+        </div>
+    </div>
+</nav>
+    <?php include 'menu.php'; ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h2>Bienvenido al sistema de gestión de palomas</h2>
+                <img src="" alt="">
+            </div>
+        </div>
+    </div>
+
+    <footer class="text-center">
+        CETIS 107 &copy; 2023 
+    </footer>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
