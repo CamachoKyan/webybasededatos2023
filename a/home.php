f3a9623a08f20b61a920cbb6df2665375ae1435e
CamachoKyan <124410885+CamachoKyan@users.noreply.github.com>
2023-10-30 19:37:43 -0700

    Add files via upload

---
+++ a/home.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+</head>
+<body>
+  <?php include 'conexion.php'; include 'menu.php'; ?>
+
+    <div class="container">
+        <div class="row">
+          <div class="card m-2">
+            <h1>Sistema de gestión de control escolar</h1>
+            <h3>CETIS 107</h3>
+            <img src="imagenes/foto1.png" alt="" width="20%" class="img-fluid">
+          </div>
+        </div>
+    </div>
+    <footer class="text-center"><hr>
+      <span>Cetis 107 web App &copy; 2023</span>
+    </footer>
+    <script src="js/bootstrap.js"></script>
+    
+</body>
+</html>
